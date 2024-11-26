418c8bc4abc43f2a8ea3d12a60ee9d11781c61d7
MatthewBott <124312330+MatthewBott@users.noreply.github.com>
2024-11-25 18:10:58 -0800

    MaxHeapOperations.java

--- MaxHeapOperations.java
+++ MaxHeapOperations.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class MaxHeapOperations
+public class MaxHeapOperations implement MaxHeapInterface
 {
     private int[] heap;
     private int size;
